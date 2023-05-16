f9d19ddb9f1ab3dbb1a9c5f41718bf6f7e4eb797
Johnathan Aspinwall <jwaspin@gmail.com>
2023-05-16 14:36:47 -0600

    Fix comment spacing

--- harvesters/src/gcmd/index.js
+++ harvesters/src/gcmd/index.js
@@ -1,4 +1,4 @@
-/*global process */
+/* global process */
 // eslint-disable-next-line no-unused-vars
 const dotenv = require('dotenv').config({ path: 'src/gcmd/.env' });
 const axios = require('axios');
