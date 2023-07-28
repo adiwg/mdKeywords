c796a9b42eefd83ae9ad4ed56dc35b5a50d5d29d
Johnathan Aspinwall <jwaspin@gmail.com>
2023-07-28 09:21:42 -0600

    Saving progress.
    New format for gcmd config.
    New parsing using different endpoints for gcmd harvester.

--- harvesters/src/gcmd.js
+++ harvesters/src/gcmd.js
@@ -1,241 +1,134 @@
 const axios = require('axios');
 const { loadConfig, sleep, writeToLocalFile } = require('./utils');
-const { ceil } = require('lodash');
-
 const CONF_JSON = 'conf/gcmd.json';
-const { categoryUrl, keywordUrl, outputFileBaseUrl, outputFilePrefix } =
-  loadConfig(CONF_JSON);
 
-const KeywordPrototype = {
-  uuid: null,
-  parentId: null,
-  label: null,
-  definition: null,
-  children: [],
-};
+const { baseUrl, outputFilePrefix, outputFileBaseUrl } = loadConfig(CONF_JSON);
 
-const parseMeta = (data) =>
-  data.split(/:(.*)/s)[1].replace('"', '').replace(/ /s, '');
-
-// Primary cell is the cell associated with the UUID
-function isPrimaryCell(keyword, headers, headerIndex) {
-  const nextHeader = headers[headerIndex + 1];
-  const nextLabel = keyword[nextHeader];
-  if (nextHeader === 'UUID') {
-    return true;
-  }
-  if (nextLabel === '') {
-    return isPrimaryCell(keyword, headers, headerIndex + 1);
-  }
-  return false;
+function generateFilename(name) {
+  return `${outputFilePrefix}${name}.json`;
 }
 
-function hasError(currentHeader, headerIndex, headersLength) {
-  return currentHeader === 'UUID' || headerIndex >= headersLength;
-}
-
-async function loadCategoryPage(id, pageNumber) {
-  const pageSize = 2000;
-  const pageUrl = `${categoryUrl}${id}/?format=csv&page_num=${pageNumber}&page_size=${pageSize}`;
-  const response = await axios.get(pageUrl).catch((err) => {
-    console.log('Error loading keywords', err);
-    return;
-  });
-  if (response.error) {
-    console.log('Response Error:', response.error);
-    return;
-  }
-  if (response.data) {
-    const lines = response.data.split('\n');
-    const categoryMetaData = lines[0].split(',');
-    const categoryHeaders = lines[1].split(',');
-    const keywordLinesRaw = lines.slice(2);
-    const hits = parseInt(parseMeta(categoryMetaData[0]));
-    const pageNum = parseInt(parseMeta(categoryMetaData[1]));
-    const totalPages = ceil(hits / pageSize);
-    const version = parseMeta(categoryMetaData[3]);
-    const revision = parseMeta(categoryMetaData[4]);
-    const timestamp = parseMeta(categoryMetaData[5]);
-    const terms = parseMeta(categoryMetaData[6]);
-    const xmlUrl = parseMeta(categoryMetaData[7]);
-    const headers = categoryHeaders;
-    const keywords = keywordLinesRaw
-      .filter((line) => line.split(',').length > 1)
-      .map((line) => {
-        const words = line.split(',');
-        const newLine = {};
-        categoryHeaders.forEach((header, i) => {
-          newLine[header] = words[i].replace(/"/g, '');
-        });
-        return newLine;
-      });
-    return {
-      id,
-      hits,
-      pageNum,
-      totalPages,
-      version,
-      revision,
-      timestamp,
-      terms,
-      xmlUrl,
-      headers,
-      keywords,
-    };
+async function fetchConceptById(id) {
+  try {
+    sleep(500);
+    const response = await axios.get(`${baseUrl}concept/${id}?format=json`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching data: ', error);
+    return null;
+    // throw error;
   }
 }
 
-async function loadCategory(vocabulary) {
+// async function fetchConceptsByScheme(scheme) {
+//   console.log('fetchConceptsByScheme(scheme)', scheme);
+//   try {
+//     const { shortName } = scheme;
+//     const response = await axios.get(
+//       `${baseUrl}concepts/concept_scheme/${shortName}?format=json&scheme=${shortName}`
+//     );
+//     return response.data;
+//   } catch (error) {
+//     console.error('Error fetching data: ', error);
+//     throw error;
+//   }
+// }
+
+async function fetchKeywordById(id) {
+  try {
+    sleep(500);
+    const response = await axios.get(`${baseUrl}keyword/${id}?format=json`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching data: ', error);
+    return null;
+    // throw error;
+  }
+}
+
+const loadMetadata = async (vocabulary) => {
   const { id, name } = vocabulary;
-  let pageNumber = 1;
-  const metadata = await loadMetadata(vocabulary);
-  const category = await loadCategoryPage(metadata.scheme, pageNumber);
-  category.id = id;
-  category.label = name;
-  const { totalPages } = category;
-  if (totalPages > 1) {
-    while (pageNumber < totalPages) {
-      await sleep(1000);
-      pageNumber += 1;
-      const nextPage = await loadCategoryPage(metadata.scheme, pageNumber);
-      category.keywords.push(...nextPage.keywords);
-    }
-  }
-  return category;
-}
-
-function findNode(node, keyword, headers, headerIndex) {
-  const label = keyword[headers[headerIndex]];
-  for (const currentNode of node.children) {
-    if (currentNode.label === label) {
-      return findNode(currentNode, keyword, headers, headerIndex + 1);
-    }
-  }
-  return { node, headerIndex };
-}
-
-function generateChildChain(node, keyword, headers, headerIndex) {
-  console.log('Generating child chain', node, keyword, headerIndex);
-  const currentHeader = headers[headerIndex];
-  const currentLabel = keyword[currentHeader];
-  if (hasError(currentHeader, headerIndex, headers.length)) {
-    console.log('Error: generateChildChain called on invalid column');
-    if (currentHeader === 'UUID') {
-      console.log('Error: currentHeader === "UUID"');
-    }
-    if (headerIndex >= headers.length) {
-      console.log('Error: headerIndex >= headers.length');
-    }
+  console.log('loadMetadata', id, name);
+  sleep(500);
+  const conceptData = await fetchConceptById(id);
+  // const schemeData = await fetchConceptsByScheme(conceptData.scheme);
+  const keywordData = await fetchKeywordById(id);
+  if (!conceptData || !keywordData) {
     return null;
   }
-  const newChildNode = Object.create(KeywordPrototype);
-  newChildNode.label = currentLabel;
-  newChildNode.children = [];
-  if (isPrimaryCell(keyword, headers, headerIndex)) {
-    newChildNode.uuid = keyword.UUID;
-  } else {
-    newChildNode.children = generateChildChain(
-      newChildNode,
-      keyword,
-      headers,
-      headerIndex + 1
-    );
-    if (newChildNode.children === null) {
-      return null;
+  return { ...vocabulary, conceptData, /*  schemeData, */ keywordData };
+};
+
+async function buildChildren(metadata) {
+  if (metadata.conceptData.isLeaf) {
+    return [];
+  }
+  const narrowerList = metadata.conceptData.narrower;
+  const children = [];
+  for (let i = 0; i < narrowerList.length; i++) {
+    const narrower = narrowerList[i];
+    const narrowerMetadata = await loadMetadata({
+      id: narrower.uuid,
+      isLeaf: narrower.isLeaf,
+    });
+    if (!narrowerMetadata) {
+      console.log('narrowerMetadata not found', narrower);
+      continue;
     }
+    const narrowerChildren = await buildChildren(narrowerMetadata);
+    children.push({
+      broader: narrowerMetadata?.conceptData?.broader[0]?.uuid || null,
+      label: narrowerMetadata.conceptData.prefLabel,
+      definition: narrowerMetadata.keywordData.definition,
+      children: narrowerChildren,
+    });
   }
-  console.log('currentLabel', currentLabel);
-  if (currentLabel === '') {
-    console.log('No Label: using children of parent node');
-    return [...node.children, ...newChildNode.children];
-  }
-  return [...node.children, newChildNode];
+  return children;
 }
 
-async function upsertKeyword(tree, keyword, headers) {
-  console.log('Upserting keyword', keyword);
-  const { node, headerIndex } = findNode(tree, keyword, headers, 0);
-  if (
-    node.label === keyword[headers[headerIndex - 1]] &&
-    keyword[headers[headerIndex]] === ''
-  ) {
-    node.uuid = keyword.UUID;
-  } else {
-    node.children = await generateChildChain(
-      node,
-      keyword,
-      headers,
-      headerIndex
-    );
-  }
-  if (node.children === null) {
-    return 'ERROR';
-  }
-  return 'OK';
-}
+const buildKeywordTree = async (metadata) => {
+  const tree = [
+    {
+      broader: null,
+      uuid: metadata.conceptData.uuid,
+      label: metadata.conceptData.prefLabel,
+      definition:
+        metadata.keywordData.definition || 'No description available.',
+      children: [await buildChildren(metadata)],
+    },
+  ];
+  return tree;
+};
 
-async function buildKeywordTree(category) {
-  console.log('buildKeywordTree(category)', category);
-  const { id, label, headers, keywords } = category;
-  const tree = {
-    id,
-    label,
-    headers,
-    children: [],
-  };
-  let status = 'OK';
-  for (const keyword of keywords) {
-    status = await upsertKeyword(tree, keyword, headers);
-    if (status === 'ERROR') break;
-  }
-  if (status === 'ERROR') return null;
-  return tree.children;
-}
-
-async function loadMetadata(vocabulary) {
-  const { id } = vocabulary;
-  const metadataUrl = `${keywordUrl}${id}`;
-  const response = await axios.get(metadataUrl).catch((err) => {
-    console.log('Error loading metadata', err);
-    return;
-  });
-  if (response.error) {
-    console.log('Response Error:', response.error);
-    return;
-  }
-  if (response.data) {
-    const metadata = response.data;
-    return metadata;
-  }
-}
-
-function generateCitationInternal(vocabulary, filename, metadata) {
+function buildCitation(metadata) {
   return {
     citation: {
       date: [
         {
-          date: metadata.changeNotes[0].date,
+          date: metadata.conceptData.schemeVersion,
           dateType: 'revision',
         },
       ],
-      description: '',
-      title: `Global Change Master Directory (GCMD) ${metadata.prefLabel}`,
-      edition: `Version ${metadata.version}`,
+      description: 'No description available.',
+      title: `Global Change Master Directory (GCMD) ${metadata.conceptData.prefLabel}`,
+      edition: `Version ${metadata.conceptData.keywordVersion}`,
       onlineResource: [
         {
-          uri: `${categoryUrl}${metadata.scheme}`,
+          uri: `${baseUrl}concept/${metadata.conceptData.uuid}?format=json}`,
         },
       ],
       identifier: [
         {
-          identifier: vocabulary.id,
+          identifier: metadata.conceptData.uuid,
         },
       ],
     },
     keywordType: metadata.scheme,
-    label: vocabulary.name,
+    label: metadata.conceptData.prefLabel,
     dynamicLoad: true,
-    keywordsUrl: `${outputFileBaseUrl}${filename}`,
+    keywordsUrl: `${outputFileBaseUrl}${generateFilename(
+      metadata.conceptData.scheme.shortName
+    )}`,
     keywords: [],
   };
 }
@@ -243,16 +136,16 @@ function generateCitationInternal(vocabulary, filename, metadata) {
 async function generateKeywordsFile(vocabulary) {
   console.log('generateKeywordsFile(vocabulary)', vocabulary);
   const metadata = await loadMetadata(vocabulary);
-  const category = await loadCategory(vocabulary);
-  const keywordsJson = await buildKeywordTree(category);
-  const filename = `${outputFilePrefix}${metadata.scheme}.json`;
+  const keywordsJson = await buildKeywordTree(metadata);
+  const filename = generateFilename(metadata.conceptData.scheme.shortName);
+  console.log('keywordsJson', keywordsJson);
   if (keywordsJson) writeToLocalFile(keywordsJson, filename);
 }
 
 async function generateCitation(vocabulary) {
   const metadata = await loadMetadata(vocabulary);
-  const filename = `${outputFilePrefix}${metadata.scheme}.json`;
-  return generateCitationInternal(vocabulary, filename, metadata);
+  const citation = buildCitation(metadata);
+  return citation;
 }
 
 module.exports = { generateCitation, generateKeywordsFile };
