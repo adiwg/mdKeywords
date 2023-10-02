5c701d045f3fd17d42a0db4bbd89575c1ed742be
Johnathan <jwaspin@users.noreply.github.com>
2023-10-02 10:57:51 -0600

    27 develop version 302 (#29)
    * 10 migrate keywords from npm package to jsdelivr (#28)
    
    * Disable GitHub Actions Schedule (#23)
    
    * Remove npm
    
    Removing all parts of the npm package including src/ and scripts/
    Moving all harvesters/ content to root directory.
    package.json replaced by harvesters/package.json
    
    * Fix File Paths
    
    * Update vocabularies-dynamic.json
    
    * Update README.md
    
    * Update vocabularies-dynamic.json
    
    * Update GitHub Action
    
    Remove reference to working directory, the working
    directory should now be the root of the repository.
    
    * Update Version package.json
    
    * Move keywords Files
    
    * New Thesaurus Configuration Files
    
    * Remove Metadata from Config File
    
    * Remove Default Vocabs File
    
    * Updates
    
    Combined config files into single file:
    main.json.
    New ScienceBase vocabularies list.
    Modified GCMD vocabularies list.
    Removed excess files.
    New main.js will replace index.js.
    Updated LCC files.
    
    * Update Package Scripts and Version
    
    * Add USGS Citation to Config
    
    * Fix Indentation
    
    * Remove Resources
    
    * Update Harvester Processes
    
    * Remove Old Control Process
    
    * Fix ScienceBase Harvester
    
    Was not accessing the baseUrl correctly.
    Remove console logs.
    
    * Remove Resources
    
    * Add Paths to Config
    
    * Update
    
    Use the config to set the path, no hardcoded paths.
    
    * Add Error Handling and Console Logs
    
    * Update
    
    Update console logs.
    Load vocabs from file using loadConfig fn in utils.
    
    * New manifest.json
    
    * Remove Console Log
    
    * New ScienceBase Citations
    
    * New ScienceBase Keywords
    
    * Update README.md
    
    * New Citations
    
    * New Keywords Files
    
    * New GCMD Citations
    
    * Add Names to GCMD Vocab Config File
    
    * New Citations and Keywords
    
    * Add LCC Vocabularies
    
    * Add Names to GCMD Vocabularies
    
    * Update Build Citation
    
    The citation's parent object should set
    keywords to null instead of an empty array.
    
    * Update Citations
    
    keywords set to null instead of empty array.
    
    * Update Citations
    
    keywords set to null instead of empty array.
    
    * Update Citations
    
    keywords set to null instead of empty array.
    
    * Add keywords key to USGS citation
    
    * Update GitHub Action
    
    * 4.0.0
    
    * Update README.md

---
+++ src/gcmd.js
@@ -0,0 +1,116 @@
+const axios = require('axios');
+const { loadConfig, sleep } = require('./utils');
+
+const CONF_JSON = 'conf/main.json';
+const { baseUrl } = loadConfig(CONF_JSON).gcmd;
+
+async function fetchConceptById(id) {
+  try {
+    const response = await axios.get(`${baseUrl}concept/${id}?format=json`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching concept data', id, error);
+    return null;
+  }
+}
+
+async function fetchKeywordById(id) {
+  try {
+    const response = await axios.get(`${baseUrl}keyword/${id}?format=json`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching keyword data', id, error);
+    return null;
+  }
+}
+
+const loadMetadata = async (vocabulary) => {
+  await sleep(50);
+  const { id } = vocabulary;
+  const conceptData = await fetchConceptById(id);
+  const keywordData = await fetchKeywordById(id);
+  return { ...vocabulary, conceptData, keywordData };
+};
+
+function generateNode(metadata) {
+  return {
+    uuid: metadata.conceptData.uuid,
+    label: metadata.conceptData.prefLabel,
+    broader: metadata?.conceptData?.broader[0]?.uuid || null,
+    definition:
+      metadata?.conceptData?.definitions[0]?.text ||
+      metadata?.keywordData?.definition ||
+      'No definition available.',
+    children: [],
+  };
+}
+
+async function buildChildren(metadata) {
+  if (metadata.conceptData.isLeaf) {
+    return [];
+  }
+  const narrowerList = metadata.conceptData.narrower;
+  const children = [];
+  for (let i = 0; i < narrowerList.length; i++) {
+    const narrower = narrowerList[i];
+    const narrowerMetadata = await loadMetadata({
+      id: narrower.uuid,
+      isLeaf: narrower.isLeaf,
+      name: narrower.prefLabel,
+    });
+    const narrowerChildren = await buildChildren(narrowerMetadata);
+    const node = generateNode(narrowerMetadata);
+    node.children = narrowerChildren;
+    children.push(node);
+  }
+  return children;
+}
+
+const buildKeywordTree = async (metadata) => {
+  const node = generateNode(metadata);
+  node.children = await buildChildren(metadata);
+  return [node];
+};
+
+function buildCitation(metadata) {
+  return {
+    citation: {
+      date: [
+        {
+          date: metadata.conceptData.schemeVersion,
+          dateType: 'revision',
+        },
+      ],
+      description: metadata.description || 'No description available.',
+      title: `Global Change Master Directory (GCMD) ${metadata.conceptData.prefLabel}`,
+      edition: `Version ${metadata.conceptData.keywordVersion}`,
+      onlineResource: [
+        {
+          uri: `${baseUrl}concept/${metadata.conceptData.uuid}?format=json}`,
+        },
+      ],
+      identifier: [
+        {
+          identifier: metadata.conceptData.uuid,
+        },
+      ],
+    },
+    keywordType: metadata?.conceptData?.scheme?.shortName,
+    label: metadata.conceptData.prefLabel,
+    keywords: null,
+  };
+}
+
+async function generateKeywords(vocabulary) {
+  const metadata = await loadMetadata(vocabulary);
+  const keywordsJson = await buildKeywordTree(metadata);
+  return keywordsJson;
+}
+
+async function generateCitation(vocabulary) {
+  const metadata = await loadMetadata(vocabulary);
+  const citation = buildCitation(metadata);
+  return citation;
+}
+
+module.exports = { generateCitation, generateKeywords };
--- src/main.js
+++ src/main.js
@@ -1,18 +1,118 @@
-/**
- * @module mdkeywords
- */
+const path = require('path');
 
-import category from '../resources/json/lcc-category.json'
-import deliverabletype from '../resources/json/lcc-deliverabletype.json'
-import usertype from '../resources/json/lcc-usertype.json'
+const sciencebase = require('./sciencebase');
+const usgs = require('./usgs');
+const gcmd = require('./gcmd');
+const { loadConfig, writeToLocalFile } = require('./utils');
 
-function asArray () {
-  return [category, deliverabletype, usertype]
+async function loadVocabulariesFromFile(filename) {
+  const vocabulariesFilePath = path.join('conf', filename);
+  const { vocabularies } = loadConfig(vocabulariesFilePath);
+  return vocabularies;
 }
-// export default [category, deliverabletype, usertype]
-export {
-  category,
-  deliverabletype,
-  usertype,
-  asArray
+
+async function loadVocabularies(config) {
+  let sbVocabularies = await loadVocabulariesFromFile(
+    config.sciencebase.filename
+  );
+  sbVocabularies = sbVocabularies.map((vocabulary) => ({
+    ...vocabulary,
+    source: 'sciencebase',
+  }));
+  let gcmdVocabularies = await loadVocabulariesFromFile(config.gcmd.filename);
+  gcmdVocabularies = gcmdVocabularies.map((vocabulary) => ({
+    ...vocabulary,
+    source: 'gcmd',
+  }));
+  let usgsVocabularies = config.usgs.vocabularies;
+  usgsVocabularies = usgsVocabularies.map((vocabulary) => ({
+    ...vocabulary,
+    source: 'usgs',
+  }));
+  const vocabularies = [
+    ...sbVocabularies,
+    ...gcmdVocabularies,
+    ...usgsVocabularies,
+  ];
+  return vocabularies;
 }
+
+async function buildManifest(vocabularies, config) {
+  const manifest = vocabularies.map((vocabulary) => ({
+    id: vocabulary.id,
+    name: vocabulary.name,
+    source: vocabulary.source,
+    citationUrl: `${config.citationsPath}/${vocabulary.source}-${vocabulary.id}.json`,
+    keywordsUrl: `${config.keywordsPath}/${vocabulary.source}-${vocabulary.id}.json`,
+  }));
+  return manifest;
+}
+
+async function buildThesaurusConfig(vocabulary) {
+  let thesaurusConfigFile;
+  switch (vocabulary.source) {
+    case 'sciencebase':
+      thesaurusConfigFile = await sciencebase.generateCitation(vocabulary);
+      break;
+    case 'usgs':
+      thesaurusConfigFile = await usgs.generateCitation(vocabulary);
+      break;
+    case 'gcmd':
+      thesaurusConfigFile = await gcmd.generateCitation(vocabulary);
+      break;
+    default:
+      throw new Error('Bad source type');
+  }
+  return thesaurusConfigFile;
+}
+
+async function buildKeywords(vocabulary) {
+  let keywords;
+  switch (vocabulary.source) {
+    case 'sciencebase':
+      keywords = await sciencebase.generateKeywords(vocabulary);
+      break;
+    case 'usgs':
+      keywords = await usgs.generateKeywords(vocabulary);
+      break;
+    case 'gcmd':
+      keywords = await gcmd.generateKeywords(vocabulary);
+      break;
+    default:
+      throw new Error('Bad source type');
+  }
+  return keywords;
+}
+
+async function main() {
+  // load configuration
+  const config = loadConfig('conf/main.json');
+
+  // load list of vocabularies
+  console.log('Loading vocabularies');
+  const vocabularies = await loadVocabularies(config);
+
+  // generate manifest file
+  console.log('Building manifest file');
+  const manifest = await buildManifest(vocabularies, config);
+  writeToLocalFile(manifest, config.manifestPath);
+
+  for (let i = 0; i < vocabularies.length; i++) {
+    // for each vocabulary create thesaurus configuration file
+    const vocabulary = vocabularies[i];
+    console.log('processing vocabulary', vocabulary.id);
+    const configData = await buildThesaurusConfig(vocabulary);
+    writeToLocalFile(
+      configData,
+      `${config.citationsPath}/${vocabulary.source}-${vocabulary.id}.json`
+    );
+    // for each vocabulary create keywords file
+    const keywords = await buildKeywords(vocabulary);
+    writeToLocalFile(
+      keywords,
+      `${config.keywordsPath}/${vocabulary.source}-${vocabulary.id}.json`
+    );
+  }
+}
+
+main();
